55038c771bde07657ead211056c1417e3e1d744d
Cyberdise <141998354+Cyberdise@users.noreply.github.com>
2023-12-01 09:46:02 +0100

    Aufgabe1

--- Day1/Main.java
+++ Day1/Main.java
@@ -1,5 +1,25 @@
 class Main {
     public static void main(String[] args){
+            File f = new File("./Day1/input.txt");
+
+            BufferedReader b = new BufferedReader(new FileReader(f));
+
+            String readLine = "";
+
+            for (int charcount = 0; charcount < readLine.lenght(); charcount++){
+                int foundInt = 0;
+                char c = readLine.charAt(charcount);
+                int first;
+                int following;
+                if (c.isDigit()){
+                    if (foundInt = 0){
+                        first = (int) c;
+                        foundInt++;
+                    }else {
+                        following = (int) c;
+                    }
+                }
+            }
 
     }
 }

957a9af57a070f8c47af00a688ba40befe6fb6d1
Cyberdise <141998354+Cyberdise@users.noreply.github.com>
2023-12-04 15:28:31 +0100

    Erster Ansatz (Work in progress)

--- Day3/Main.java
+++ Day3/Main.java
@@ -1,5 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        String lastString = null;
+        String prevprevString = null;
+        String prevString = null;
+        String currString = null;
+        File f = new File("./input.txt");
+        BufferedReader b = new BufferedReader(new FileReader(f));
+        boolean init = true;
+        String readLine = "";
+        while ((readLine = b.readLine()) != null){
+            if (init){
+                readLine = currString;
+                init = false;
+            }else{
+                prevprevString = prevString;
+                prevString = currString;
+                currString = readLine;      
+            }
+            String buffer = null;
+            int startIndex = 0;
+            int endIndex = 0;
+
+            for(char ch: prevString.toCharArray()){
+                int countIndex = 0;
+                String courser = "" + ch;
+                
+                if (isNumeric(courser)){
+                    startIndex = countIndex;
+                }else{
+                    countIndex++;
+                }
+            }
+            
+        }
 
     }
+
+    public static boolean isNumeric(String str) { 
+        try {  
+          Double.parseDouble(str);  
+          return true;
+        } catch(NumberFormatException e){  
+          return false;  
+        }  
+      }
 }

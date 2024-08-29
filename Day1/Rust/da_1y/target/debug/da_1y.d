fa947d0df802ca2e65c1e44089648c0a23299f05
Aleksandr Paramoshin <141998354+Cyberdise@users.noreply.github.com>
2024-08-29 12:58:38 +0200

    init

---
+++ Day1/Rust/da_1y/target/debug/da_1y.d
@@ -0,0 +1,1 @@
+/media/alex/ssd/GitHub/Repositories/AdventOfCode2023/Day1/Rust/da_1y/target/debug/da_1y: /media/alex/ssd/GitHub/Repositories/AdventOfCode2023/Day1/Rust/da_1y/src/main.rs
---
+++ Day1/Rust/da_1y/target/debug/deps/da_1y-06d21e6381033dd3.d
@@ -0,0 +1,5 @@
+/media/alex/ssd/GitHub/Repositories/AdventOfCode2023/Day1/Rust/da_1y/target/debug/deps/libda_1y-06d21e6381033dd3.rmeta: src/main.rs
+
+/media/alex/ssd/GitHub/Repositories/AdventOfCode2023/Day1/Rust/da_1y/target/debug/deps/da_1y-06d21e6381033dd3.d: src/main.rs
+
+src/main.rs:
